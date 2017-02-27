4a5fc46ef5fe0b3232ab6eb542e4e27d49d7fd11
Jérémy  VOLLANT <jeremy.vollant@altran.com>
2017-02-27 11:59:55 +0100

    all done

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,14 +1,22 @@
+// A container is a normal React Component that gets bounded to
+// the application state
+// Whenever application state changes, the container will re-render as well
+
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { selectBook } from '../actions/index'; //action creator
 import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
-
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li
+          key={book.title}
+          className="list-group-item"
+          onClick={() => this.props.selectBook(book)}>
+          {book.title}
+        </li>
       );
     });
   }
@@ -20,23 +28,34 @@ class BookList extends Component {
       </ul>
     );
   }
-
 }
 
-function mapStateToProps(state) {
+
+////////////////////////////////////////////////////////////////////////////////
+//                                BookList Display                            //
+////////////////////////////////////////////////////////////////////////////////
+function mapStateToProps(state) { // state here is the overall application state
   // Whatever is returned will show up as props inside of BookList
   return {
     books: state.books
   };
-}
+} // this.props.books
 
-// Anything returned from this function will end up as props on the BookList container
+////////////////////////////////////////////////////////////////////////////////
+//                                Book Selected: action                       //
+////////////////////////////////////////////////////////////////////////////////
+// Anything returned from this function will end up as props
+// on the BookList container
 function mapDispatchToProps(dispatch) {
-// Whenever selectBook is called, the result should be passed to all of our reducers
-  return bindActionsCreators({ selectBook: selectbook}, dispatch)
-}
+// Whenever selectBook is called, the result should be passed
+// to all of our reducers
+  return bindActionCreators({ selectBook: selectBook}, dispatch)
+} // this.props.selectBook
 
 
+////////////////////////////////////////////////////////////////////////////////
+//                       Component to Container promotion                     //
+////////////////////////////////////////////////////////////////////////////////
 // Promote BookList from a Component to a Container - it needs to know
 // about this new dispatch method, selectBook. Make it available as a prop.
 export default connect(mapStateToProps, mapDispatchToProps)(BookList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
-import BooksReducer from './reducer_books'
+import BooksReducer from './reducer_books';
+import ActiveBook from './reducer_active_book';
 
 const rootReducer = combineReducers({
-  books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBook
   // this reducer is going to add a key to our global application state called "books",
   // where the key is "books" and the value is whatever gets returned by the book reducer
   // which in this case, is our array of books
